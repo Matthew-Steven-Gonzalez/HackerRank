4c67f49946b513f6393b5910b6037225b57b0180
Matthew <gonzalez.matthew.steven@gmail.com>
2023-03-29 20:54:06 -0400

    "HackerRanks"

---
+++ JavaScript/10daysofJS.js
@@ -0,0 +1,30 @@
+"USE STRICT";
+
+// Day 1:
+
+print("Hello World");
+
+
+// Day 0 Data Types;
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    console.log( firstInteger + parseInt(secondInteger));
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    console.log( firstString  + secondString);
+}
+
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
+
+
+
+
