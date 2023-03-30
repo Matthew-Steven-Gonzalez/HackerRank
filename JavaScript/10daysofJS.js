bf85b1b8a16d387dfb560ea929a697afad362449
Matthew <gonzalez.matthew.steven@gmail.com>
2023-03-30 13:04:39 -0400

    "added a new day"

--- JavaScript/10daysofJS.js
+++ JavaScript/10daysofJS.js
@@ -52,5 +52,29 @@ function main() {
     console.log(2 * PI * r);
 }
 
+// Day 2: else If
 
+function getGrade(score) {
+    let grade;
+    
+    if(score <= 5){
+        grade = "F";
+    }
+    else if(score <= 10){
+        grade = "E";
+    }
+    else if(score <= 15){
+        grade = "D";
+    }
+    else if(score <=20){
+        grade = "C";
+    }
+    else if(score <=25){
+        grade = "B";
+    }
+    else{
+        grade = "A";
+    }
+    return grade;
+}
 
