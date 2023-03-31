854501f256d9272ff9e699638317bb69db5e073d
Matthew <gonzalez.matthew.steven@gmail.com>
2023-03-31 09:33:36 -0400

    continued with day 3

--- JavaScript/10daysofJS.js
+++ JavaScript/10daysofJS.js
@@ -151,6 +151,53 @@ function getSecondLargest(nums) {
     return secondLargest;
 }
 
+// Day 3 Try,Catch, and Finally
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+
+function reverseString(s) {
+    
+    try{
+        console.log(s.split("").reverse().join(""));
+    
+    } catch(error){
+        console.log(error.message);
+        console.log(s);
+    }
+    
+}
+
+// Day 3 
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    
+    try{
+        if( a == 0){
+            throw new Error('Zero Error');
+        }
+        
+        if(a < 0){
+            throw new Error('Negative Error')
+        }
+        
+        if(a > 0){
+            return 'YES';
+        }
+        
+    } catch(error){
+        throw error;
+    }
+    
+}
 
 
 
