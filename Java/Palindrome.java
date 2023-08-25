2439e9e8602064254d4c831de5b22f7d1ce4fcb1
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-08-25 09:13:22 -0400

    added details

--- Java/Palindrome.java
+++ Java/Palindrome.java
@@ -9,12 +9,13 @@ public class Palindrome {
         /* Enter your code here. Print output to STDOUT. */
         sc.close();
 
+        // string builder to create empty string for reverse string
         StringBuilder B = new StringBuilder();
-
+        // adds the exsisting string to stringbuilder object
         B.append(A);
-
+        // reversed the order of the string
         B.reverse();
-
+        // if logic to check if strings equal each other.
         if (A.equals(B.toString())) {
             System.out.println("Yes");
         } else {
