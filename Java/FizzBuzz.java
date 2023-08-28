e0f61bdc4204aa00e1c405d4ae0e2c7aaad5ddbd
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-08-28 17:57:02 -0400

    completed practice fizzbuzz

---
+++ Java/FizzBuzz.java
@@ -0,0 +1,25 @@
+import java.util.function.Function;
+
+public class FizzBuzz {
+
+    public static void fizzBuzz() {
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 3 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz();
+    }
+
+}
